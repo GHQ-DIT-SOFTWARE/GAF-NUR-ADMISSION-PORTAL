f92c652321a03cdb9e116b6dc7424e852550715b
Isaac-Totimeh <isaactotimeh10@gmail.com>
2025-05-14 12:11:38 +0000

    update

--- resources/views/portal/master.blade.php
+++ resources/views/portal/master.blade.php
@@ -9,7 +9,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="description" content="" />
     <meta name="keywords" content="">
-
     <meta name="author" content="Phoenixcoded" />
     <link rel="icon" href="{{ asset('logo-icon.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('frontend/assets/css/plugins/flipclock.css') }}">
@@ -20,7 +19,7 @@
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.css"
         integrity="sha512-CbQfNVBSMAYmnzP3IC+mZZmYMP2HUnVkV4+PwuhpiMUmITtSpS7Prr3fNncV1RBOnWxzz4pYQ5EAGG4ck46Oig=="
-        crossorigin="anonymous" rel="stylesheet" />
+        crossorigin="anonymous" rel="stylesheet"/>
 </head>
 <div class="content-wrapper">
     @yield('content')
